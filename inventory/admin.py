9cbbf0854c90dd004ec911284b7dba8bde6fce26
Vagner Guedes <vagnerrguedess@gmail.com>
2020-08-02 17:35:19 -0300

    Exporting books to xlsx

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,5 +1,30 @@
+from openpyxl import Workbook
+from openpyxl.writer.excel import save_virtual_workbook
+
 from django.contrib import admin
+from django.http import HttpResponse
 
 from inventory.models import Book
 
-admin.site.register(Book)
+
+class BookAdmin(admin.ModelAdmin):
+    actions = ['download_xlsx']
+    actions_selection_counter = True
+    list_filter = ('author__first_name',)
+    search_fields = ['title', 'author__first_name', 'author__last_name', 'author__username']
+    
+    def download_xlsx(self, request, queryset):
+        workbook = Workbook()
+        worksheet = workbook.active
+        worksheet.append(['id', 'title'])
+        for book in queryset.order_by('id'):
+            worksheet.append([book.id, book.title])
+        response = HttpResponse(
+            content=save_virtual_workbook(workbook),
+            content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+        )
+        response['Content-Disposition'] = 'attachment; filename=myexport.xlsx'
+        return response
+    download_xlsx.short_description = 'Download selected books'
+
+admin.site.register(Book, BookAdmin)
--- inventory/models.py
+++ inventory/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Book(models.Model):
     title = models.CharField(max_length=255)
-    author = models.ManyToManyField(User)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
